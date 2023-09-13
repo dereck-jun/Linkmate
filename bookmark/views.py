a0173d3fa71fbdd212384a3470a4fa8633d806f8
박용준 <park-yongjun@dereck.local>
2023-09-13 22:55:34 +0900

    CBV로 북마크 리스트 페이지 만들기

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from .models import Bookmark
-# Create your views here.
-def index(request):
-  bookmarks = Bookmark.objects.all().order_by('title')
-  
-  return render(request, 'bookmark/index.html', {'bookmarks': bookmarks})
+from django.views.generic import ListView
 
+# Create your views here.
+class BookmarkList(ListView):
+  model = Bookmark
+  ordering = "title"
+  
 def single_list_page(request, pk):
   bookmark = Bookmark.objects.get(pk=pk)
   
