f29af456760275fab39d36daae683e8741590cf4
박용준 <park-yongjun@dereck.local>
2023-09-13 22:29:26 +0900

    FBV로 북마크 리스트 페이지 만들기

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Bookmark
 # Create your views here.
+def index(request):
+  bookmarks = Bookmark.objects.all().order_by('title')
+  
+  return render(request, 'bookmark/index.html', {'bookmarks': bookmarks})
