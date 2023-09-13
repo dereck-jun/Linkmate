a52ed1a866ec43b9e86dd53741b6f0350332a875
박용준 <park-yongjun@dereck.local>
2023-09-13 22:42:56 +0900

    FBV로 북마크 리스트 상세 페이지 만들기

--- bookmark/views.py
+++ bookmark/views.py
@@ -5,3 +5,8 @@ def index(request):
   bookmarks = Bookmark.objects.all().order_by('title')
   
   return render(request, 'bookmark/index.html', {'bookmarks': bookmarks})
+
+def single_list_page(request, pk):
+  bookmark = Bookmark.objects.get(pk=pk)
+  
+  return render(request, 'bookmark/single_list_page.html', {'bookmark': bookmark})
