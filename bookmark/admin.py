5150ef40055cdd73f2ae927efe2cf4a8b2488720
박용준 <park-yongjun@dereck.local>
2023-09-14 14:13:04 +0900

    북마크 카테고리 admin 창에서 만들기

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Bookmark
+from .models import Bookmark, Category
 # Register your models here.
 
 admin.site.register(Bookmark)
+
+class CategoryAdmin(admin.ModelAdmin):
+  prepopulated_fields = {'slug': ('name', )}
+  
+admin.site.register(Category, CategoryAdmin)
--- bookmark/models.py
+++ bookmark/models.py
@@ -4,6 +4,18 @@ import os
 
 
 # Create your models here.
+
+class Category(models.Model):
+  name = models.CharField(max_length=50, unique=True)
+  slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+  
+  def __str__(self):
+    return self.name
+  
+  class Meta:
+    verbose_name_plural = 'Categories'
+
+
 class Bookmark(models.Model):
   title = models.CharField(max_length=60)
   url = models.URLField(max_length=120)
@@ -15,6 +27,8 @@ class Bookmark(models.Model):
   updated_at = models.DateTimeField(auto_now=True)
   
   author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+  
+  category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
   
   def __str__(self):
     return f'[{self.pk}]{self.title} :: {self.author}'
