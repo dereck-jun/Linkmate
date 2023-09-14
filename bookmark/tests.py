d1e716578a1733b9da3d5dcf22419d3880811251
박용준 <park-yongjun@dereck.local>
2023-09-14 12:44:32 +0900

    북마크 목록, 상세 페이지에서 작성자 정보 출력하기

--- bookmark/tests.py
+++ bookmark/tests.py
@@ -1,10 +1,14 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
+from django.contrib.auth.models import User
 from .models import Bookmark
 # Create your tests here.
 class TestView(TestCase):
   def setUp(self):
     self.client = Client()
+    self.user_trump = User.objects.create_user(username='trump', password='somepassword')
+    self.user_obama = User.objects.create_user(username='obama', password='somepassword')
+    
     
   def navbar_test(self, soup):
     navbar = soup.nav
@@ -33,14 +37,33 @@ class TestView(TestCase):
     
     self.navbar_test(soup)
     
-  def test_post_detail(self):
-    bookmark_000 = Bookmark.objects.create(
+    self.assertEqual(Bookmark.objects.count(), 0)
+    main_area = soup.find('div', id='main-area')
+    self.assertIn('아직 북마크가 없습니다', main_area.text)
+    
+    bookmark_001 = Bookmark.objects.create(
       title="네이버",
       url='www.naver.com',
+      author=self.user_trump
     )
-    self.assertEqual(bookmark_000.get_absolute_url(), '/bookmark/1/')
-    res = self.client.get(bookmark_000.get_absolute_url())
-    self.assertEqual(res.status_code, 200)
+    bookmark_002 = Bookmark.objects.create(
+      title="구글",
+      url='www.google.com',
+      author=self.user_obama
+    )
+    
+    self.assertEqual(Bookmark.objects.count(), 2)
+    
+    res = self.client.get('/bookmark/')
     soup = BeautifulSoup(res.content, 'html.parser')
     
-    self.navbar_test(soup)
+    self.assertEqual(res.status_code, 200)
+    soup = BeautifulSoup('div', id='main-area')
+    
+    self.assertIn(bookmark_001.title, main_area.text)
+    self.assertIn(bookmark_002.title, main_area.text)
+    self.assertNotIn('아직 게시물이 없습니다', main_area.text)
+    
+    self.assertIn(self.user_trump.username.upper(), main_area.text)
+    self.assertIn(self.user_obama.username.upper(), main_area.text)
+    
