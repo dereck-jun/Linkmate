24d4a083eab320464a65850da9c73851a5cadf8c
dereck <yjpark0805@naver.com>
2023-09-17 20:13:50 +0900

    카테고리 페이지 만들기

--- bookmark/views.py
+++ bookmark/views.py
@@ -13,6 +13,21 @@ class BookmarkList(ListView):
     
     return context
   
+def category_page(request, slug):
+  if slug == 'no_category':
+    category = '미분류'
+    bookmark_list = Bookmark.objects.filter(category=None)
+  else:
+    category = Category.objects.get(slug=slug)
+    bookmark_list = Bookmark.objects.filter(category=category)
+  
+  return render(request, 'bookmark/bookmark_list.html', {
+    'bookmark_list': bookmark_list,
+    'categories': Category.objects.all(),
+    'no_category_bookmark_count': Bookmark.objects.filter(category=None).count(),
+    'category': category,
+  })
+  
   
 class BookmarkDetail(DetailView):
   model = Bookmark
@@ -23,3 +38,5 @@ class BookmarkDetail(DetailView):
     context['no_category_post_count'] = Bookmark.objects.filter(category=None).count()
     
     return context
+  
+  
