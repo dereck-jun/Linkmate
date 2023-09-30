0fe3d9e78d53d4142f24ebeb9a8a8c55172ddd1d
박용준 <park-yongjun@dereck.local>
2023-09-30 22:50:37 +0900

    pagination으로 북마크 나눠서 보여주기

--- bookmark/views.py
+++ bookmark/views.py
@@ -40,6 +40,7 @@ class BookmarkCreate(LoginRequiredMixin, UserPassesTestMixin ,CreateView):
 class BookmarkList(ListView):
   model = Bookmark
   ordering = "title"
+  paginate_by = 5
   
   def get_context_data(self, *, object_list=None, **kwargs):
     context = super(BookmarkList, self).get_context_data()
